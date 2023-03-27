fc150f177d6d6042e7083deb6a0b1540eee120a3
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-27 14:45:46 -0700

    printf project

--- print.c
+++ print.c
@@ -20,7 +20,7 @@ void cleanup(va_list args, buffer_t *output)
  * @format: Character string to print - may contain directives.
  * @output: A buffer_t struct containing a buffer.
  * @args: A va_list of arguments.
- *
+ * @ return: An int  value for the main printf function
  * Return: The number of characters stored to output.
  */
 int run_printf(const char *format, va_list args, buffer_t *output)
@@ -28,8 +28,8 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	int i, wid, prec, ret = 0;
 	char tmp;
 	unsigned char flags, len;
-	unsigned int (*f)(va_list, buffer_t *,
-			unsigned char, int, int, unsigned char);
+	unsigned int (*f)(va_list, buffer_t *, unsigned char, int, int,
+			unsigned char);
 
 	for (i = 0; *(format + i); i++)
 	{
