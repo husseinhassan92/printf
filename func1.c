44f4e974de94288e337f638212edbc533f8af91a
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-03-27 14:20:18 -0700

    printf project

--- func1.c
+++ func1.c
@@ -1,16 +1,10 @@
 #include "main.h"
-
 unsigned int convert_c(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_percent(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_p(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_s(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_b(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-
 
 /**
  * convert_c - Converts an argument to an unsigned char and
@@ -102,71 +96,4 @@ unsigned int convert_p(va_list args, buffer_t *output,
 	ret += print_neg_width(output, ret, flags, wid);
 
 	return (ret);
-}
-/**
- * convert_s - Converts an argument to a string and
- *             stores it to a buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
- *
- * Return: The number of bytes stored to the buffer.
- */
-unsigned int convert_s(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len)
-{
-	char *str, *null = "(null)";
-	int size;
-	unsigned int ret = 0;
-
-	(void)flags;
-	(void)len;
-
-	str = va_arg(args, char *);
-	if (str == NULL)
-		return (_memcpy(output, null, 6));
-
-	for (size = 0; *(str + size);)
-		size++;
-
-	ret += print_string_width(output, flags, wid, prec, size);
-
-	prec = (prec == -1) ? size : prec;
-	while (*str != '\0' && prec > 0)
-	{
-		ret += _memcpy(output, str, 1);
-		prec--;
-		str++;
-	}
-
-	ret += print_neg_width(output, ret, flags, wid);
-
-	return (ret);
-}
-
-/**
- * convert_b - Converts an unsigned int argument to binary
- *             and stores it to a buffer contained in a struct.
- * @args: A va_list pointing to the argument to be converted.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
- * @len: A length modifier.
- * @output: A buffer_t struct containing a character array.
- *
- * Return: The number of bytes stored to the buffer.
- */
-unsigned int convert_b(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len)
-{
-	unsigned int num;
-
-	num = va_arg(args, unsigned int);
-
-	(void)len;
-
-	return (convert_ubase(output, num, "01", flags, wid, prec));
 }
